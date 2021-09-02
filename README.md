881c79b5315a0dbab9306dd5a79324ddcb7d5df9
Alex Sung <alexsung98@gmail.com>
2021-09-02 16:55:48 -0700

    Update README, and all source files
    Quote Generator project completed! 
    Functionality is all working as expected, tested quote tweeting functionality on Twitter page and works as expected. Added a loader icon to spin during the first fetch request when no quotes are available.

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# quote-generator
-JS Web Project Quote Generator
+# Quote Generator
+Find your inspiration! 
 
-A single page web project using HTML, CSS, and JS to generate random quotes to a user. The page consists of a floating box in the center of the page with two buttons, one to tweet the current quote to a logged in Twitter account, and the second quote executes an asynchronous JS function call that fetches and retrieves new quotes from a source API. 
+## About
+Generate random motivational or inspirational quotes with the click of a button. The page consists of a floating box in the center of the page with two buttons, one to tweet the current quote to a logged in Twitter account, and the second quote executes an asynchronous JS function call that fetches and retrieves new quotes from a source API. 
 
-# Resources
-Quotes API URL: https://type.fit/api/quotes
+## Built With
+
+### Front End
+ - HTML5
+ - CSS3
+ - JavaScript
+
+### APIs
+ - Typefit Quotes
+ - Forismatic getQuote
+
+## Resources
+Quotes API URL: https://type.fit/api/quotes, http://api.forismatic.com/api/1.0/
 Background wallpaper: https://www.heropatterns.com/
 Fonts: https://fonts.google.com/
 Tweet Button Integration Documentation: https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent
--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with 'Unknown'
@@ -22,12 +36,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes'
     try {
         // set up fetch request
@@ -39,6 +55,19 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    // template string using backticks to use variable content
+    // ? provides a query with text = ${} insert variable. 
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); // _blank allows to open in new tab
+}
+
+// Event Listeners
+// usually at bottom bc need to declare function before calling
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
